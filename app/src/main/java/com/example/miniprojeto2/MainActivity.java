75d7128baeebe008651522af024f5a063015ef7e
j175343 <j175343@dac.unicamp.br>
2023-10-05 12:07:28 -0300

    Initial commit

---
+++ app/src/main/java/com/example/miniprojeto2/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.miniprojeto2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] frases = {"frase boa", "frase legal", "frase mais ou menos"};
+    private TextView fraseViewer;
+    /*private Button btFrase;*/
+    Button btFrase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fraseViewer = findViewById(R.id.fraseViewerXML);
+        btFrase = (Button) findViewById(R.id.btFraseXML);
+
+        // Criar listener para botao "btFrase" e
+        // fazer a textview "fraseViewer" receber string do array "frases"
+        // em um endereço aleatorio com o método random.java
+
+        btFrase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation respostaBT = AnimationUtils.loadAnimation(MainActivity.this, R.anim.blink_anim);
+                btFrase.startAnimation(respostaBT); // Resolver Animação em Loop
+                int f = new Random().nextInt(frases.length);
+                fraseViewer.setText(frases[f]);
+            }
+        });
+
+    }
+
+
+    /*public void gerarFrase(View view) {
+
+        fraseViewer = findViewById(R.id.fraseViewerXML);
+        btFrase = findViewById(R.id.btFraseXML);
+
+        // Criar listener para botao "btFrase" e
+        // fazer a textview "fraseViewer" receber string do array "frases"
+        // em um endereço aleatorio com o método random.java
+
+        btFrase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int f = new Random().nextInt(2);
+                fraseViewer.setText(frases[f]);
+            }
+        });
+
+    }*/
+}
